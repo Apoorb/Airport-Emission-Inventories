43bbc0e49b55a2a86438bc0d20bffd8eefb5a7f1
Apoorba Bibeka <apoorb2510@gmail.com>
2021-06-30 18:03:00 -0500

    apply black

--- analysis/01_explore_nfdc_facilities_counties.py
+++ analysis/01_explore_nfdc_facilities_counties.py
@@ -5,29 +5,34 @@ from airportei.utilis import PATH_RAW, PATH_INTERIM, get_snake_case_dict
 
 
 if __name__ == "__main__":
-    path_nfdc = os.path.join(PATH_RAW, "madhu_files",
-                                   "FAA_NFDC_Facilities.csv")
+    path_nfdc = os.path.join(PATH_RAW, "madhu_files", "FAA_NFDC_Facilities.csv")
     nfdc_facilities = pd.read_csv(path_nfdc)
     nfdc_rename_dict = get_snake_case_dict(nfdc_facilities)
     # path_airport = os.path.join(PATH_RAW, "Airports.csv")
     # airport = pd.read_csv(path_airport)
     path_airport_shp = os.path.join(PATH_RAW, "Airports", "Airports.shp")
     airport = gpd.read_file(path_airport_shp)
-    path_counties_shp = os.path.join(PATH_RAW,
-                                 "Texas_County_Boundaries_Detailed-shp",
-                                 "County.shp")
+    path_counties_shp = os.path.join(
+        PATH_RAW, "Texas_County_Boundaries_Detailed-shp", "County.shp"
+    )
     counties_tx = gpd.read_file(path_counties_shp)
+    assert airport.crs == counties_tx.crs, (
+        "Make sure the coordinate reference system is same between the "
+        "geodataframes before joining."
+    )
     counties_tx_1 = (
         counties_tx.rename(columns=get_snake_case_dict(counties_tx))
-        .rename(columns={"cnty_nm": "county", "dist_nm": "district_tx_boundar",
-                         "cnty_fips": "fips_tx_boundar"})
-        .assign(
-            county=lambda df: (
-                df.county.str.strip().str.lower().str.replace("'", "")
-            ),
+        .rename(
+            columns={
+                "cnty_nm": "county",
+                "dist_nm": "district_tx_boundar",
+                "cnty_fips": "fips_tx_boundar",
+            }
         )
-        .filter(items=["county", "district_tx_boundar", "fips_tx_boundar",
-                       "geometry"])
+        .assign(
+            county=lambda df: (df.county.str.strip().str.lower().str.replace("'", ""))
+        )
+        .filter(items=["county", "district_tx_boundar", "fips_tx_boundar", "geometry"])
     )
 
     nfdc_facilities_1 = (
@@ -84,9 +89,7 @@ if __name__ == "__main__":
     nfdc_facilities_1_fil = nfdc_facilities_1.filter(
         items=["facility_id", "facility_name", "city", "county"]
     ).assign(
-        county=lambda df: (
-            df.county.str.strip().str.lower().str.replace("'", "")
-        ),
+        county=lambda df: (df.county.str.strip().str.lower().str.replace("'", "")),
         facility_id=lambda df: (
             df.facility_id.str.strip().str.lower().str.replace("'", "")
         ),
@@ -96,11 +99,7 @@ if __name__ == "__main__":
     )
 
     airport_tx_fil_counties = (
-        gpd.sjoin(
-            airport_tx,
-            counties_tx_1,
-            how="left",
-            op="intersects")
+        gpd.sjoin(airport_tx, counties_tx_1, how="left", op="intersects")
         .rename(
             columns={
                 "ident": "facility_id",
@@ -122,8 +121,9 @@ if __name__ == "__main__":
         .assign(
             facility_id=lambda df: df.facility_id.replace(
                 ["1e4", "1e2", "1e7", "2e5", "2e7", "3e0"],
-                [str(int(i)) for i in [1e4, 1e2, 1e7, 2e5, 2e7, 3e0]])
-    )
+                [str(int(i)) for i in [1e4, 1e2, 1e7, 2e5, 2e7, 3e0]],
+            )
+        )
         .filter(
             items=[
                 "facility_id",
@@ -135,7 +135,7 @@ if __name__ == "__main__":
                 "operstatus_arpt",
                 "lat_arpt",
                 "long_arpt",
-                "geometry"
+                "geometry",
             ]
         )
     )
@@ -148,14 +148,14 @@ if __name__ == "__main__":
         how="outer",
     )
 
-    nfdc_arpt_mrg["qc"] = (nfdc_arpt_mrg.county_arpt
-                           == nfdc_arpt_mrg.county_nfdc).astype(int)
+    nfdc_arpt_mrg["qc"] = (
+        nfdc_arpt_mrg.county_arpt == nfdc_arpt_mrg.county_nfdc
+    ).astype(int)
 
     path_out = os.path.join(PATH_INTERIM, "nfdc_vs_arpt_city_comp.xlsx")
     nfdc_arpt_mrg.to_excel(path_out)
     path_out_tx_counties = os.path.join(PATH_INTERIM, "tx_counties.csv")
-    counties_tx_1.drop("geometry", axis=1).to_csv(
-        path_out_tx_counties)
+    counties_tx_1.drop("geometry", axis=1).to_csv(path_out_tx_counties)
 
     miss_nfdc = nfdc_arpt_mrg.loc[lambda df: df.city_nfdc.isna()]
 
@@ -163,6 +163,7 @@ if __name__ == "__main__":
 
     mask = (nfdc_arpt_mrg.county_nfdc.isna()) | (nfdc_arpt_mrg.county_arpt.isna())
     assert all(
-        nfdc_arpt_mrg.loc[~mask, "county_nfdc"] == nfdc_arpt_mrg.loc[~mask, "county_arpt"]
+        nfdc_arpt_mrg.loc[~mask, "county_nfdc"]
+        == nfdc_arpt_mrg.loc[~mask, "county_arpt"]
     )
     operation_date = nfdc_facilities_1.operations_date.value_counts().sort_index()
