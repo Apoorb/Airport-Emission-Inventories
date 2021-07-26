84141e264be3f6f1fff1d515933b481066a2111a
Apoorba Bibeka <apoorb2510@gmail.com>
2021-07-26 14:18:08 -0400

    placeholder ops creation scripts.

---
+++ analysis/vii_prepare_ops_for_asif.py
@@ -0,0 +1,38 @@
+"""
+
+"""
+
+
+def get_flt_db_tabs():
+    ...
+
+
+def get_arptdummy_trks_layout():
+    ...
+
+
+def ops_prep():
+    ...
+
+
+def add_eng_arfm_equip_cols():
+    ...
+
+def add_profiles():
+    ...
+
+
+def add_apu_nms():
+    ...
+
+
+def split_ops_arrdep():
+    ...
+
+
+def get_asif_input_tab():
+    ...
+
+
+if __name__ == "__main__":
+    ...
