fc62c93fafd0bf9ed32fde16a97fd4a9e3722818
Apoorba Bibeka <apoorb2510@gmail.com>
2021-08-16 21:16:59 -0500

    apply black

--- test/test_fleetmix.py
+++ test/test_fleetmix.py
@@ -11,10 +11,7 @@ ops_2019_fil = ops_2019.loc[
     lambda df: (df.facility_group.isin(["Commercial", "Reliever"]))
 ]
 
-path_out_emis_comm_rel = Path.home().joinpath(PATH_PROCESSED,
-                                         "emis_comm_releiv.xlsx")
-path_out_flt = Path.home().joinpath(PATH_PROCESSED, "fleet_comm_releiv.xlsx")
-path_out_emis = Path.home().joinpath(PATH_PROCESSED,
-                                     "emis_non_comm_releiv.xlsx")
-path_out_flt = Path.home().joinpath(PATH_PROCESSED,
-                                    "fleet_non_comm_releiv.xlsx")
+path_out_emis_comm_rel = Path.home().joinpath(PATH_PROCESSED, "emis_comm_reliev.xlsx")
+path_out_flt = Path.home().joinpath(PATH_PROCESSED, "fleet_comm_reliev.xlsx")
+path_out_emis = Path.home().joinpath(PATH_PROCESSED, "emis_non_comm_reliev.xlsx")
+path_out_flt = Path.home().joinpath(PATH_PROCESSED, "fleet_non_comm_reliev.xlsx")
