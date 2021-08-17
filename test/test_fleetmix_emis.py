f08d11938b169581d21152269b9423010810b92c
Apoorba Bibeka <apoorb2510@gmail.com>
2021-08-17 13:00:36 -0500

    test processing

---
+++ test/test_fleetmix_emis.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from airportei.utilis import PATH_PROCESSED, PATH_INTERIM
+
+# FixME: Clean up the global variables---more to pytest fixtures.
+path_ops2019_clean = Path.home().joinpath(
+    PATH_INTERIM, "ops2019_meta_imputed_cor_counties.xlsx"
+)
+ops_2019 = pd.read_excel(path_ops2019_clean, index_col=0)
+ops_2019_fil = ops_2019.loc[lambda df: (~df.facility_type.isin(["GLIDERPORT"]))]
+EXPECTED_FAC_IDS = ops_2019_fil.facility_id.unique()
+
+path_out_emis_comm_rel = Path.home().joinpath(PATH_PROCESSED, "emis_comm_reliev.xlsx")
+path_out_flt_comm_rel = Path.home().joinpath(PATH_PROCESSED, "fleet_comm_reliev.xlsx")
+path_out_emis_non_comm_rel = Path.home().joinpath(
+    PATH_PROCESSED, "emis_non_comm_reliev.xlsx"
+)
+path_out_flt_non_comm_rel = Path.home().joinpath(
+    PATH_PROCESSED, "fleet_non_comm_reliev.xlsx"
+)
+
+emis_comm_rel = pd.read_excel(path_out_emis_comm_rel)
+flt_comm_rel = pd.read_excel(path_out_flt_comm_rel)
+emis_non_comm_rel = pd.read_excel(path_out_emis_non_comm_rel)
+flt_non_comm_rel = pd.read_excel(path_out_flt_non_comm_rel)
+flt_df = pd.concat([flt_comm_rel, flt_non_comm_rel])
+emis_df = pd.concat([emis_comm_rel, emis_non_comm_rel])
+
+
+def test_all_fac_id_present():
+    """
+    There are 2,032 operational airport facilities in Texas. 5010 data shows
+    2,037 airports. We have not considered the 5 GLIDERPORT in this
+    inventory. Check with Madhu about the justification.
+    """
+    comm_rel_fac = emis_comm_rel.facility_id.unique()
+    non_comm_rel_fac = emis_non_comm_rel.facility_id.unique()
+    fac_output = set(comm_rel_fac).union(non_comm_rel_fac)
+    assert fac_output == set(EXPECTED_FAC_IDS)
+
+
+def test_all_fleetmix_in_flt_eq_1():
+    """
+    The sum of fleetmix should add-up to 1.
+    """
+    assert np.allclose(flt_df.groupby(["facility_id"]).fleetmix.sum().values, 1)
+
+
+def test_sum_ltos_time_2_eq_ops():
+    """
+    LTOs = Number of operations / 2. Check if the sum of ltos * 2 by facility
+    is equal to the input operations.
+    """
+    flt_df_ltos = flt_df.groupby("facility_id").ltos.sum() * 2
+    input_ops = ops_2019_fil[["facility_id", "annual_operations"]].set_index(
+        "facility_id"
+    )
+    test_df = pd.merge(flt_df_ltos, input_ops, left_index=True, right_index=True)
+    assert np.allclose(test_df.annual_operations, test_df.ltos)
+
+
+def test_emis_fleet_eq_flt_fleet():
+    """
+    Test that emission data fleetmix, ltos, and operations are equal to the
+    fleetmix
+    data.
+    """
+    emis_df["tfmsc_aircraft_id"] = emis_df["tfmsc_aircraft_id"].fillna(-99)
+    emis_df_fleetmix = (
+        emis_df.loc[lambda df: ~df.fleetmix.isna()]
+        .groupby(["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"])
+        .agg(fleetmix=("fleetmix", "mean"), ops=("ops", "mean"), ltos=("ltos", "mean"))
+        .reset_index()
+    )
+    flt_df["tfmsc_aircraft_id"] = flt_df["tfmsc_aircraft_id"].fillna(-99)
+
+    test_df = emis_df_fleetmix.merge(
+        flt_df,
+        on=["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"],
+        suffixes=["_emis", "_flt"],
+        how="outer",
+    )
+
+    t1 = test_df[~np.isclose(test_df["ops_emis"], test_df["ops_flt"])]
+    assert (np.allclose(test_df["fleetmix_emis"], test_df["fleetmix_flt"])) & (
+        np.allclose(test_df["ops_emis"], test_df["ops_flt"])
+        & (np.allclose(test_df["ltos_emis"], test_df["ltos_flt"]))
+    )
+
+
+def test_emis_unique_cats_eq_4():
+    """
+    Test that there are only 4 unique categories
+    """
+    emis_df["tfmsc_aircraft_id"] = emis_df["tfmsc_aircraft_id"].fillna(-99)
+    emis_df_fil = emis_df.loc[lambda df: ~df.Mode.isin(["GSE LTO", "APU"])]
+    mask = (
+        emis_df_fil.groupby(
+            ["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"]
+        ).facility_id.transform("count")
+        != 4
+    )
+    t1 = emis_df_fil.loc[mask]
+    t1["counts"] = (
+        t1.groupby(["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"])
+        .facility_id.transform("count")
+        .values
+    )
+    t1.sort_values(
+        ["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"], inplace=True
+    )
+    all(
+        emis_df.groupby(
+            ["facility_id", "airframe_id", "engine_id", "tfmsc_aircraft_id"]
+        ).facility_id.count()
+        == 4
+    )
+    all(
+        emis_df.groupby(["facility_id", "tfmsc_aircraft_id"]).facility_id.count().values
+        == 4
+    )
+
+
+def test_fleetmix_approx_eq_tfmsc():
+    ...
+
+
+def test_farm_and_ranch_fleetmix_eq_0_2():
+    ...
+
+
+def test_heliport_fleetmix_eq_0_3333():
+    ...
